c22791d6e6e86472b9e8e86005010ab9cb50d735
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2018-04-17 11:19:43 +0200

    Revert "Feature/frontend basis"

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import {Provider} from 'react-redux';
-
-import store from './store';
-import AppRouter from './AppRouter';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}><AppRouter/></Provider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
