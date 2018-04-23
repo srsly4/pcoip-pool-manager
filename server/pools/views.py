125eda63d6c462af10f325f93a01546867703ae1
Michał Zakrzewski <miczak96@gmail.com>
2018-04-23 11:26:30 +0200

    Created API for login

--- server/pools/views.py
+++ server/pools/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+import json
+
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+
 
 # Create your views here.
 
+
+def login(request):
+    """
+    View responsible for logging user by the given credentials using Django session manager
+    :param request: HTTPRequest, body made of JSON containing fields "username" and "password"
+    :return: 200 if user exists
+    :raise: 404 if login fails due to unrecognized username or password
+    """
+    if request.method == 'POST':
+        try:
+            json_body = json.loads(request.body)
+            username, password = json_body['username'], json_body['password']
+            assert username is not None and password is not None
+        except (TypeError, AssertionError, KeyError):
+            return HttpResponseBadRequest("Incorrect request body")
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            # django.contrib.auth.login(request, user)
+            return HttpResponse(status=200)
+        else:
+            return HttpResponseNotFound("Login failed")
+
+
+def reservation(request):
+    pass
