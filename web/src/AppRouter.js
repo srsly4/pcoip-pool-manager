6e52098b632924b877b0f199b3a50892e663c41e
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2018-04-24 08:36:58 +0200

    Added additional route for pools

--- web/src/AppRouter.js
+++ web/src/AppRouter.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {HashRouter} from 'react-router-dom';
 import EnsureLoggedInPath from './components/EnsureLoggedInPath';
 import HomeView from './components/HomeView';
+import PoolsView from './components/PoolsView';
 
 export default class AppRouter extends React.Component {
   render() {
       return (<HashRouter>
-        <EnsureLoggedInPath exact path="/" component={HomeView} />
+        <div>
+          <EnsureLoggedInPath exact path="/" component={HomeView} />
+          <EnsureLoggedInPath exact path="/pools" component={HomeView} />
+        </div>
       </HashRouter>);
   }
 }
