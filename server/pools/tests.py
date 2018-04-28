4a57b81e4a76a5de1e021b41cfc78ebad4d4e1ac
Michał Zakrzewski <miczak96@gmail.com>
2018-04-28 13:38:28 +0200

    Recreated tests to use Django REST Framework
    WARNING: Login tests aren't working, please check why

--- server/pools/tests.py
+++ server/pools/tests.py
@@ -6,7 +6,8 @@ from django.test import TestCase, Client
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
 from .models import Pool, Reservation
-from .views import login
+from .views import Authentication
+
 
 # Create your tests here.
 
@@ -20,8 +21,9 @@ class ModelTest(TestCase):
                                    end_datetime=datetime(2018, 4, 1, 12, 45))
 
     def test_already_reserved_slots(self):
-        self.assertEqual(self.pool.calculate_already_reserved_slots(datetime(2018, 4, 1, 11, 00), datetime(2018, 4, 1, 12, 30)),
-                         5)
+        self.assertEqual(
+            self.pool.calculate_already_reserved_slots(datetime(2018, 4, 1, 11, 00), datetime(2018, 4, 1, 12, 30)),
+            5)
 
     def test_can_place_reservation(self):
         self.assertTrue(self.pool.can_place_reservation(3, datetime(2018, 4, 1, 11, 00), datetime(2018, 4, 1, 13, 00)))
@@ -31,46 +33,55 @@ class ModelTest(TestCase):
 class LoginTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(username="test", password="testpassword", is_active=True)
-        self.user.set_password("testpassword")
         self.user.save()
         self.client = Client()
         self.factory = APIRequestFactory()
 
-    def test_login_post(self):
-        correct_request = self.factory.post(path="/login/", data={"username": "test", "password": "testpassword"},
-                                            format="json")
-        incorrect_request = self.factory.post(path="/login/", data={"field": "test"}, format="json")
-        no_such_user_request = self.factory.post(path="/login/", data={"username": "tset", "password": "tset"},
-                                                 format="json")
-        self.assertEqual(login(correct_request).status_code, status.HTTP_200_OK)
-        self.assertEqual(login(incorrect_request).status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(login(no_such_user_request).status_code, status.HTTP_404_NOT_FOUND)
+    def test_login_correct(self):
+        correct_request = self.factory.post("/auth/", json.dumps({"username": "test", "password": "testpassword"}),
+                                            content_type="application/json")
+        print(correct_request.body)
+        self.assertEqual(self.client.post(correct_request).status_code, status.HTTP_200_OK)
+
+    def test_login_incorrect(self):
+        incorrect_request = self.factory.post("/auth/", json.dumps({"field": "test"}),
+                                              content_type="application/json")
+        self.assertEqual(self.client.post(incorrect_request).status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_no_such_user(self):
+        no_such_user_request = self.factory.post("/auth/", json.dumps({"username": "tset", "password": "tset"}),
+                                                 content_type="application/json")
+        self.assertEqual(self.client.post(no_such_user_request).status_code, status.HTTP_404_NOT_FOUND)
 
 
 class PoolsTest(TestCase):
     def setUp(self):
-        self.pool1 = Pool.objects.create(pool_id='id1', displayName='name1', maximumCount=10,
-                                         enabled=True, description='desc1')
-        self.pool2 = Pool.objects.create(pool_id='id2', displayName='name2', maximumCount=20,
-                                         enabled=False, description='desc2')
+        self.pool1 = Pool.objects.create(pool_id="id1", displayName="name1", maximumCount=10,
+                                         enabled=True, description="desc1")
+        self.pool2 = Pool.objects.create(pool_id="id2", displayName="name2", maximumCount=20,
+                                         enabled=False, description="desc2")
         self.pool1.save()
         self.pool2.save()
 
     def test_get(self):
         data = self.client.get("/pools/")
-        self.assertEquals(200, data.status_code)
-        j = json.loads(data.content.decode())['pools']
-        p1 = {"pool_id": self.pool1.pool_id, "displayName": self.pool1.displayName, "maximumCount": self.pool1.maximumCount,
+        self.assertEquals(status.HTTP_200_OK, data.status_code)
+        j = json.loads(data.content.decode())["pools"]
+        p1 = {"pool_id": self.pool1.pool_id, "displayName": self.pool1.displayName,
+              "maximumCount": self.pool1.maximumCount,
               "enabled": self.pool1.enabled, "description": self.pool1.description}
-        p2 = {"pool_id": self.pool2.pool_id, "displayName": self.pool2.displayName, "maximumCount": self.pool2.maximumCount,
+        p2 = {"pool_id": self.pool2.pool_id, "displayName": self.pool2.displayName,
+              "maximumCount": self.pool2.maximumCount,
               "enabled": self.pool2.enabled, "description": self.pool2.description}
         self.assertTrue(p1 in j)
         self.assertTrue(p2 in j)
 
     def test_post(self):
-        p1 = {"pool_id": self.pool1.pool_id, "displayName": self.pool1.displayName, "maximumCount": self.pool1.maximumCount,
+        p1 = {"pool_id": self.pool1.pool_id, "displayName": self.pool1.displayName,
+              "maximumCount": self.pool1.maximumCount,
               "enabled": self.pool1.enabled, "description": self.pool1.description}
-        p2 = {"pool_id": self.pool2.pool_id, "displayName": self.pool2.displayName, "maximumCount": self.pool2.maximumCount,
+        p2 = {"pool_id": self.pool2.pool_id, "displayName": self.pool2.displayName,
+              "maximumCount": self.pool2.maximumCount,
               "enabled": self.pool2.enabled, "description": self.pool2.description}
         p3 = {"pool_id": "id3", "displayName": "disp3name", "maximumCount": 50,
               "enabled": False, "description": "description3"}
@@ -78,11 +89,10 @@ class PoolsTest(TestCase):
               "enabled": True, "description": "description4"}
         js = json.dumps({"pools": [p3, p4]})
         data = self.client.post("/pools/", js, content_type="application/json")
-        self.assertEquals(200, data.status_code)
+        self.assertEquals(status.HTTP_201_CREATED, data.status_code)
         data = self.client.get("/pools/")
-        j = json.loads(data.content.decode())['pools']
+        j = json.loads(data.content.decode())["pools"]
         self.assertFalse(p1 in j)
         self.assertFalse(p2 in j)
         self.assertTrue(p3 in j)
         self.assertTrue(p4 in j)
-
