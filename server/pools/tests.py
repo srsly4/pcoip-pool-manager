9b8cf16d179780091bec6051b6a386427e051d0d
Michał Zakrzewski <miczak96@gmail.com>
2018-05-12 11:56:15 +0200

    Removed commented code

--- server/pools/tests.py
+++ server/pools/tests.py
@@ -34,22 +34,20 @@ class LoginTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="test", password="testpassword", is_active=True)
         self.user.save()
-        #self.client = Client()
-        #self.factory = APIRequestFactory()
 
     def test_login_correct(self):
         correct_request = self.client.post("/auth/", json.dumps({"username": "test", "password": "testpassword"}),
-                                            content_type="application/json")
+                                           content_type="application/json")
         self.assertEqual(correct_request.status_code, status.HTTP_200_OK)
 
     def test_login_incorrect(self):
         incorrect_request = self.client.post("/auth/", json.dumps({"field": "test"}),
-                                              content_type="application/json")
+                                             content_type="application/json")
         self.assertEqual(incorrect_request.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_login_no_such_user(self):
         no_such_user_request = self.client.post("/auth/", json.dumps({"username": "tset", "password": "tset"}),
-                                                 content_type="application/json")
+                                                content_type="application/json")
         self.assertEqual(no_such_user_request.status_code, status.HTTP_404_NOT_FOUND)
 
 
