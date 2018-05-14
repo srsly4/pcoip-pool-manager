e9b85fcd4425a02eb637d1fe481c35ab4c472544
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2018-05-14 23:27:50 +0200

    Fixed travis deploy #6

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 
-docker build -f docker/Dockerfile --pull -t "pcoip-pool-manager:latest"
+docker build -f docker/Dockerfile -t "pcoip-pool-manager:latest" .
 
 docker tag "pcoip-pool-manager:latest" "srsly4/pcoip-pool-manager:latest"
 docker push "srsly4/pcoip-pool-manager:latest"
