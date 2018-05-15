49fdc3762a812722fbb9c8d271d92c8732a7fd9a
Krzys <navix9@wp.pl>
2018-05-15 11:53:24 +0200

    Add server api to PoolsView and change view

--- server/pcoippoolmanager/settings.py
+++ server/pcoippoolmanager/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PASSWORD': 'admin',
         'PORT': 5432,
     }
