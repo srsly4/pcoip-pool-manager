ae339d709826f7e84de0bf745f81bbfbd7c38ec9
Michał Zakrzewski <miczak96@gmail.com>
2018-05-18 12:17:17 +0200

    Minor change

--- server/pools/urls.py
+++ server/pools/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'pools'
 urlpatterns = [
     path('auth/', views.Authentication.as_view()),
-    path('pools/', views.PoolsList.as_view())
+    path('pools/', views.PoolsList.as_view()),
 ]
