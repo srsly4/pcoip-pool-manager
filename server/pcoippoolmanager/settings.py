dd88d3a813c82d5abea5d38f6e46c6b049653e35
Michał Zakrzewski <miczak96@gmail.com>
2018-05-22 10:57:17 +0200

    Changed 'localhost' to 'db'

--- server/pcoippoolmanager/settings.py
+++ server/pcoippoolmanager/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PASSWORD': 'admin',
         'PORT': 5432,
     }
